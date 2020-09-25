ac0e401517b2453aaee46b6f89f6e978ad9e3026
김나단 <paintime@naver.com>
2020-09-25 16:05:52 +0900

    주석 동기화

--- ArrayList.h
+++ ArrayList.h
@@ -3,14 +3,17 @@
 class ArrayList
 {
 public:
+	// 积己磊
 	ArrayList();
 	ArrayList(ArrayList& other);
 	ArrayList(int capacity);
 
+	// 加己
 	const size_t Capacity() { return m_capacity; }
 	const size_t Count() { return m_count; }
 	int& Item(int index) { return m_items[index]; }
 
+	// 皋辑靛
 	int Add(int value);
 	void AddRange(ArrayList& other);
 	void Insert(int index, int value);
