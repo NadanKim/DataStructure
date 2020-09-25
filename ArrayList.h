7dc575ea402f063456995ee31216a50f82f1bc15
김나단 <paintime@naver.com>
2020-09-25 16:32:30 +0900

    주석 처리

--- ArrayList.h
+++ ArrayList.h
@@ -1,16 +1,19 @@
-#pragma once
+﻿#pragma once
 
 class ArrayList
 {
 public:
+	// 생성자
 	ArrayList();
 	ArrayList(ArrayList& other);
 	ArrayList(int capacity);
 
+	// 속성
 	const size_t Capacity() { return m_capacity; }
 	const size_t Count() { return m_count; }
 	int& Item(int index) { return m_items[index]; }
 
+	// 메소드
 	int Add(int value);
 	void AddRange(ArrayList& other);
 	void Insert(int index, int value);
