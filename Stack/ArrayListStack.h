d742d4e957d671515f4d89016a037b7ceb572828
김나단 <paintime@naver.com>
2020-11-18 20:48:13 +0900

    ArrayListStack 생성

---
+++ Stack/ArrayListStack.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <xutility>
+#include <iostream>
+
+class ArrayListStack
+{
+public:
+#pragma region 생성자
+	ArrayListStack(int capacity = 10);
+	ArrayListStack(const ArrayListStack& other);
+#pragma endregion
+
+#pragma region 속성
+	const size_t Count() { return m_count; }
+#pragma endregion
+
+#pragma region 메서드
+	void Push(int value);
+
+	int Peek();
+	int Pop();
+	void Clear();
+
+	bool Contains(int value);
+#pragma endregion
+
+private:
+#pragma region 변수
+	size_t m_count;
+	int* m_items;
+#pragma endregion
+};
--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
 ﻿#include <iostream>
-#include "LinkedList/DoublyCircularLinkedList.h"
 
 // 생성한 자료구조 테스트용 메인
 int main()
 {
-	DoublyCircularLinkedList a;
-	a.AddFirst(1);
-	a.AddFirst(2);
-	a.AddFirst(3);
-	a.PrintInfo();
 
-	DoublyCircularLinkedListNode* newNode = new DoublyCircularLinkedListNode(4);
-	a.AddLast(newNode);
-	a.PrintInfo();
-
-	a.Insert(4, 5);
-	a.PrintInfo();
-
-	a.AddLast(1);
-	a.PrintInfo();
-
-	a.Clear();
-	a.PrintInfo();
 }
